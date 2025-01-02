53c53db6eb4f29538d0d24bad38e361752e2caaa
sudhanshu6481 <sudhanshu.gupta@cloudsufi.com>
2025-01-02 11:47:26 +0530

    added list and dictionary code

---
+++ Dictionary.py
@@ -0,0 +1,4 @@
+country = {'Afganistan':'Kabul', 'Albania':'Tirana', 'Algeria':'Algiers'}
+
+for key, value in country.items():
+    print(f"{key}: {value}")
---
+++ lists.py
@@ -0,0 +1,4 @@
+fruits= list(('kivi', 'green apple', 'pine apple', 'orange', 'stawberry', 'cranberry'))
+
+for item in fruits:
+    print(item)
